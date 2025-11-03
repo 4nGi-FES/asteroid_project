24602f292dff492ded805704a8edff4a5aec102b
4nGi-FES <official4ngi@gmail.com>
2025-11-02 21:25:28 -0500

    B: main.py clock and fps setting

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -11,10 +14,8 @@ def main():
         screen.fill("black")
         pygame.display.flip()
 
-
-#    print("Starting Asteroids!")
-#    print(f"Screen width: {SCREEN_WIDTH}")
-#    print(f"Screen height: {SCREEN_HEIGHT}")
+        #limit the fps
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
